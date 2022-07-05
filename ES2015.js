376bba89a2f8d2ae51bb28c802f75d160251c7c8
Garrett McQuigg <garrettmcquigg@gmail.com>
2022-07-05 16:34:14 -0500

    first commit

---
+++ ES2015.js
@@ -0,0 +1,13 @@
+// var PI = 3.14;
+// PI = 42;
+
+const PI = 3.14;
+
+// 1. var is function scope while let is block scope. Var can also be redeclared while let and const both cannot be.
+
+// 2. const is block scope like let, and cannot be redeclared or reassigned.
+
+// 3. The difference between let and const is; let can be reassigned but not redeclared, while const cannot be reassigned or redeclared.
+
+// 4. Hoisting refers to how variables are treated. Variables will be moved to the top of the scope they are declared in. 
+
